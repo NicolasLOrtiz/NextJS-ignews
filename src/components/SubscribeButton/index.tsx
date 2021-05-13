5e29822a462fde0f3242da81c814a97e2a7f7bda
Nícolas Lopes Ortiz <70731847+NicolasLOrtiz@users.noreply.github.com>
2021-05-13 19:47:41 -0300

    Commit Final

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -5,6 +5,7 @@ import { getStripeJs } from '../../services/stripe-js'
 
 import styles from './styles.module.scss'
 import 'react-toastify/dist/ReactToastify.css';
+import { useRouter } from 'next/dist/client/router'
 
 interface SubscribeButtonProps {
 	priceId: string;
@@ -12,11 +13,17 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps){
 	const [session] = useSession()
+	const router = useRouter();
 
 	async function handleSubscribe() {
 		if (!session) {
-			signIn('github')
-			return
+			signIn('github');
+			return;
+		}
+
+		if (session.activeSubscription) {
+			router.push('/posts');
+			return;
 		}
 
 		// Criação da Checkout session
